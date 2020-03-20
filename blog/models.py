572e5a50c685426a42a95f0cb15f30e47ef6f106
aniketAnvekar <aniketanvekar25@gmail.com>
2020-03-20 16:01:31 +0530

    Blog updated and polished

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
     title = models.CharField(max_length = 50)
     pub_date = models.DateTimeField()
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:65]
+
+    def customized_date(self):
+        return self.pub_date.strftime('%b %e %Y')
